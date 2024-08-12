25d2998147aeafbbb590daabf7feb1d11dd1a0a8
Santiago Sala <pipo0716@gmail.com>
2024-08-12 19:15:08 +0300

    Addint texture to the line renderer

--- Assets/Scripts/LineRendererBrush.cs
+++ Assets/Scripts/LineRendererBrush.cs
@@ -7,7 +7,13 @@ public class LineRendererBrush : MonoBehaviour
 {
     [Header("Elements")]
     [SerializeField] private LineRenderer _lineRendererPrefab;
+
+    [Header("Settings")]
+    [SerializeField] private Color _brushColor;
+    [SerializeField] private float _minimumDistanceBetweenPoints = 0.1f;
+
     private LineRenderer _currentLineRenderer;
+    private Vector2 _lastMouseClickPostion;
 
     // Start is called before the first frame update
     void Start()
@@ -27,25 +33,7 @@ public class LineRendererBrush : MonoBehaviour
         }
     }
 
-    private void PaintOnPaintingCanvas()
-    {
-        if (_currentLineRenderer == null)
-        {
-            return;
-        }
- 
-        RaycastHit raycastHit;
-        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycastHit, 20);
-
-        if (raycastHit.collider == null)
-        {
-            return;
-        }
-
-        AddPoint(raycastHit.point);
-
-
-    }
+    
 
     private void CreateLine()
     {
@@ -59,10 +47,37 @@ public class LineRendererBrush : MonoBehaviour
 
         _currentLineRenderer = Instantiate(_lineRendererPrefab, raycastHit.point, Quaternion.identity, transform);
 
-        _currentLineRenderer.SetPosition(0, raycastHit.point);       
+        _currentLineRenderer.SetPosition(0, raycastHit.point);
+        _currentLineRenderer.startColor = _brushColor;
+        _currentLineRenderer.endColor = _currentLineRenderer.startColor;
+
+        _lastMouseClickPostion = Input.mousePosition;
     }
 
+    private void PaintOnPaintingCanvas()
+    {
+        if (_currentLineRenderer == null)
+        {
+            return;
+        }
 
+        if(Vector2.Distance(_lastMouseClickPostion, Input.mousePosition) < _minimumDistanceBetweenPoints )
+        {
+            return;
+        }
+
+        _lastMouseClickPostion = Input.mousePosition;
+
+        RaycastHit raycastHit;
+        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycastHit, 20);
+
+        if (raycastHit.collider == null)
+        {
+            return;
+        }
+
+        AddPoint(raycastHit.point);
+    }
 
     private void AddPoint(Vector3 worldPosition)
     {
