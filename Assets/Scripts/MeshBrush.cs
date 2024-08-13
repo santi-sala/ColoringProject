26075db4745ff3fcc9f0ece617bb8994c750110b
Santiago Sala <pipo0716@gmail.com>
2024-08-13 18:26:17 +0300

    Spawning meshes on click

--- Assets/Scripts/MeshBrush.cs
+++ Assets/Scripts/MeshBrush.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +7,45 @@ public class MeshBrush : MonoBehaviour
 {
     [Header("Elements")]
     [SerializeField] private MeshFilter _meshFilterPrefab;
+
     [Header("Settings")]
+    [SerializeField] private float _brushSize;
     private Mesh _mesh;
     // Start is called before the first frame update
     void Start()
     {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CreateMesh();
+        }
+}
+
+    private void CreateMesh()
+    {
+        RaycastHit raycastHit;
+        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycastHit, 20);
+
+        if (raycastHit.collider == null)
+        {
+            return;
+        }
+
         _mesh = new Mesh();
 
         Vector3[] vertices = new Vector3[4];
         int[] triangles = new int[6];
 
-        vertices[0] = Vector3.up;
-        vertices[1] = Vector3.zero;
-        vertices[2] = Vector3.left;
-        vertices[3] = vertices[0] + Vector3.left;
+        vertices[0] = raycastHit.point + (Vector3.up + Vector3.right) * _brushSize / 2;
+        vertices[1] = vertices[0] + Vector3.down * _brushSize;
+        vertices[2] = vertices[1] + Vector3.left * _brushSize;
+        vertices[3] = vertices[2] + Vector3.up * _brushSize;
 
         triangles[0] = 0;
         triangles[1] = 1;
@@ -41,12 +67,5 @@ public class MeshBrush : MonoBehaviour
 
         MeshFilter meshFilterInstance = Instantiate(_meshFilterPrefab, transform);
         meshFilterInstance.sharedMesh = _mesh;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
